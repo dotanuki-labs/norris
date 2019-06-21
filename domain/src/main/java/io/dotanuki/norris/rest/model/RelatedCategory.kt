07da69513a803195a4bd0d0ad6750ceb8b59318e
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2019-06-22 01:56:24 +0200

    Available should be a data class

--- domain/src/main/java/io/dotanuki/norris/rest/model/RelatedCategory.kt
+++ domain/src/main/java/io/dotanuki/norris/rest/model/RelatedCategory.kt
@@ -1,6 +1,6 @@
 package io.dotanuki.norris.rest.model
 
 sealed class RelatedCategory {
-    class Available(val name: String) : RelatedCategory()
+    data class Available(val name: String) : RelatedCategory()
     object Missing : RelatedCategory()
 }
--- infrastructure/rest-chucknorris/src/main/java/io/dotanuki/norris/rest/ChuckNorrisDotIO.kt
+++ infrastructure/rest-chucknorris/src/main/java/io/dotanuki/norris/rest/ChuckNorrisDotIO.kt
@@ -1,3 +1,17 @@
-package io.dotanuki.norris.domain
+package io.dotanuki.norris.rest
 
-interface ChuckNorrisRestService
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+internal interface ChuckNorrisRestAPI {
+
+    @GET("/jokes/categories")
+    suspend fun categories(): List<RawCategories>
+
+    @GET("/jokes/search")
+    suspend fun search(@Query("query") query: String): List<RawSearch>
+
+    companion object {
+        const val API_URL = "https://api.chucknorris.io"
+    }
+}
