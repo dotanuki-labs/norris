e92c29489698c9edf6114693bf153c2d02294a9d
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2019-06-23 12:22:11 +0200

    Defined Kodein Module for infra

---
+++ infrastructure/rest-chucknorris/src/main/java/io/dotanuki/norris/rest/di/restInfrastructureModule.kt
@@ -0,0 +1,41 @@
+package io.dotanuki.norris.rest.di
+
+import io.dotanuki.norris.networking.RetrofitBuilder
+import io.dotanuki.norris.rest.ChuckNorrisDotIO
+import io.dotanuki.norris.rest.FactsInfrastructure
+import io.dotanuki.norris.rest.services.RemoteFactsService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+val restInfrastructureModule = Kodein.Module("rest-infrastructure") {
+
+    bind() from singleton {
+
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val okHttp = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+
+        val retrofit = RetrofitBuilder(
+            apiURL = ChuckNorrisDotIO.API_URL,
+            httpClient = okHttp
+        )
+
+        retrofit.create(ChuckNorrisDotIO::class.java)
+    }
+
+    bind<RemoteFactsService>() with provider {
+        FactsInfrastructure(
+            rest = instance()
+        )
+    }
+
+}
