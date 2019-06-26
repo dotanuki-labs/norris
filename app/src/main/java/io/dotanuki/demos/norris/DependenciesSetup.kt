feb7b0e366fa49f748451d5155e83747bb647edb
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2019-06-26 05:43:45 +0200

    Improved naming

--- app/src/main/java/io/dotanuki/demos/norris/DependenciesSetup.kt
+++ app/src/main/java/io/dotanuki/demos/norris/DependenciesSetup.kt
@@ -1,13 +1,13 @@
 package io.dotanuki.demos.norris
 
 import io.dotanuki.demos.norris.logger.loggerModule
-import io.dotanuki.norris.facts.di.factsModule
 import io.dotanuki.norris.domain.di.restInfrastructureModule
+import io.dotanuki.norris.facts.di.factsModule
 import org.kodein.di.conf.ConfigurableKodein
 
-object KodeinSetup {
+object DependenciesSetup {
 
-    val kodein by lazy {
+    val container by lazy {
         ConfigurableKodein(mutable = true).apply {
             addImport(loggerModule)
             addImport(restInfrastructureModule)
--- app/src/main/java/io/dotanuki/demos/norris/NorrisApplication.kt
+++ app/src/main/java/io/dotanuki/demos/norris/NorrisApplication.kt
@@ -1,10 +1,9 @@
 package io.dotanuki.demos.norris
 
 import android.app.Application
-import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 
 class NorrisApplication : Application(), KodeinAware {
 
-    override val kodein: Kodein = KodeinSetup.kodein
+    override val kodein = DependenciesSetup.container
 }
