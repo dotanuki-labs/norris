7757535bc9fb3d7aeb159a0828fc4d89ee03736c
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2019-06-30 00:20:36 +0200

    Declaring search module for Kodein

---
+++ features/search/src/main/java/io/dotanuki/norris/search/di/searchModule.kt
@@ -0,0 +1,39 @@
+package io.dotanuki.norris.search.di
+
+import io.dotanuki.norris.architecture.StateMachine
+import io.dotanuki.norris.architecture.TaskExecutor
+import io.dotanuki.norris.domain.ComposeSearchOptions
+import io.dotanuki.norris.features.utilties.ConfigChangesAwareStateContainer
+import io.dotanuki.norris.features.utilties.KodeinTags
+import io.dotanuki.norris.search.SearchPresentation
+import io.dotanuki.norris.search.SearchViewModel
+import kotlinx.coroutines.Dispatchers
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+
+val searchModule = Kodein.Module("search") {
+
+    bind() from provider {
+
+        val usecase = ComposeSearchOptions(
+            searches = instance(),
+            facts = instance()
+        )
+
+        val stateContainer = ConfigChangesAwareStateContainer<SearchPresentation>(
+            host = instance(KodeinTags.hostActivity)
+        )
+
+        val stateMachine = StateMachine(
+            container = stateContainer,
+            executor = TaskExecutor.Concurrent(
+                scope = stateContainer.emissionScope,
+                dispatcher = Dispatchers.IO
+            )
+        )
+
+        SearchViewModel(usecase, stateMachine)
+    }
+}
