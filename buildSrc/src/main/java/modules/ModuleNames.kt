2dc6324b59cc5b93159160fa5f90c8b7202fbb33
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2019-06-30 03:35:25 +0200

    Flattening modules structure

--- buildSrc/src/main/java/modules/ModuleNames.kt
+++ buildSrc/src/main/java/modules/ModuleNames.kt
@@ -8,17 +8,17 @@ object ModuleNames {
     const val CoroutinesTestUtils = ":coroutines-testutils"
 
     object Infrastructure {
-        const val Networking = ":infrastructure:networking"
-        const val Persistance = ":infrastructure:persistance"
-        const val Rest = ":infrastructure:rest-chucknorris"
+        const val Networking = ":networking"
+        const val Persistance = ":persistance"
+        const val Rest = ":rest-chucknorris"
     }
 
     object Features {
-        const val Architecture = ":features:architecture"
-        const val SharedAssets = ":features:shared-assets"
-        const val SharedUtilities = ":features:shared-utilities"
-        const val Facts = ":features:facts"
-        const val Search = ":features:search"
-        const val Navigator = ":features:navigator"
+        const val Architecture = ":unidirectional-dataflow"
+        const val SharedAssets = ":shared-assets"
+        const val SharedUtilities = ":shared-utilities"
+        const val Facts = ":facts"
+        const val Search = ":search"
+        const val Navigator = ":navigator"
     }
 }
