b920132aa66813c082e0866ea8c23c283aa5f813
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2019-07-05 23:09:43 +0200

    Removed unused method

--- coroutines-testutils/src/main/java/io/dotanuki/coroutines/testutils/suspendableUtils.kt
+++ coroutines-testutils/src/main/java/io/dotanuki/coroutines/testutils/suspendableUtils.kt
@@ -1,14 +1,6 @@
 package io.dotanuki.coroutines.testutils
 
-import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
-
 fun unwrapError(result: Result<*>) =
     result.exceptionOrNull()
         ?.let { it }
         ?: throw IllegalArgumentException("Not an error")
-
-suspend fun <T> errorAtSuspendableOperation(error: Throwable) =
-    suspendCoroutine<T> { continuation ->
-        continuation.resumeWithException(error)
-    }
