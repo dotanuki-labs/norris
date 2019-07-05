06cd921bdc12a4c7e2e08263d756dbe86237d1d9
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2019-07-05 23:01:16 +0200

    Implemented Categories cache infrastructure

--- persistance/src/main/java/io/dotanuki/norris/persistance/di/persistanceModule.kt
+++ persistance/src/main/java/io/dotanuki/norris/persistance/di/persistanceModule.kt
@@ -1,12 +1,15 @@
 package io.dotanuki.norris.persistance.di
 
+import io.dotanuki.norris.domain.services.CategoriesCacheService
 import io.dotanuki.norris.domain.services.SearchesHistoryService
 import io.dotanuki.norris.persistance.AppPreferencesWrapper
+import io.dotanuki.norris.persistance.CachedCategoriesInfrastructure
 import io.dotanuki.norris.persistance.SearchHistoryInfrastructure
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
 
 val persistanceModule = Kodein.Module("persistance") {
 
@@ -17,5 +20,9 @@ val persistanceModule = Kodein.Module("persistance") {
         )
 
         SearchHistoryInfrastructure(wrapper.preferences)
+    }
+
+    bind<CategoriesCacheService>() with singleton {
+        CachedCategoriesInfrastructure
     }
 }
