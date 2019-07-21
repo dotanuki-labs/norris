0a57c6e1655db4372bfd5879350460e2573d636d
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2019-07-22 00:08:11 +0200

    Definining default test scope

--- facts/src/main/java/io/dotanuki/norris/facts/NewSearch.kt
+++
@@ -1,5 +0,0 @@
-package io.dotanuki.norris.facts
-
-import io.dotanuki.norris.architecture.UserInteraction
-
-data class NewSearch(val query: String) : UserInteraction.Feature()
--- search/src/main/java/io/dotanuki/norris/search/ValidateQuery.kt
+++
@@ -1,5 +0,0 @@
-package io.dotanuki.norris.search
-
-import io.dotanuki.norris.architecture.UserInteraction
-
-data class ValidateQuery(val query: String) : UserInteraction.Feature()
