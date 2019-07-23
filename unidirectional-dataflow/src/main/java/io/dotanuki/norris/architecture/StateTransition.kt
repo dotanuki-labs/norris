20b17729b2f1e43eea9fd82e8398326c53b9d78b
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2019-07-24 01:18:50 +0200

    Improving design

--- unidirectional-dataflow/src/main/java/io/dotanuki/norris/architecture/StateTransition.kt
+++ unidirectional-dataflow/src/main/java/io/dotanuki/norris/architecture/StateTransition.kt
@@ -4,11 +4,22 @@ sealed class StateTransition<out T> {
 
     interface Parameters
 
-    class Unparametrized<T>(val task: suspend () -> T) : StateTransition<T>()
-    class Parametrized<T>(val task: suspend (Parameters) -> T, val parameters: Parameters) : StateTransition<T>()
+    class Unparametrized<T> internal constructor(
+        val task: suspend () -> T
+    ) : StateTransition<T>()
+
+    class Parametrized<T> internal constructor(
+        val task: suspend (Parameters) -> T,
+        val parameters: Parameters
+    ) : StateTransition<T>()
 
     companion object {
+
         operator fun <T> invoke(task: suspend () -> T) = Unparametrized(task)
-        operator fun <T> invoke(task: suspend (Parameters) -> T, data: Parameters) = Parametrized(task, data)
+
+        operator fun <T> invoke(
+            task: suspend (Parameters) -> T,
+            data: Parameters
+        ) = Parametrized(task, data)
     }
 }
