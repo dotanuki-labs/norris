ed6ecbed76555ed54d1cbb1cc45f7ef031bd1e9c
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-08-09 21:35:45 -0300

    Refactored tests for ManageSearchQuery with fakes

--- platform/domain/src/test/java/io/dotanuki/norris/domain/ManageSearchQueryTests.kt
+++ platform/domain/src/test/java/io/dotanuki/norris/domain/ManageSearchQueryTests.kt
@@ -1,35 +1,40 @@
 package io.dotanuki.norris.domain
 
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
 import io.dotanuki.norris.domain.services.SearchesHistoryService
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Before
 import org.junit.Test
 
 class ManageSearchQueryTests {
 
-    private val searchHistory = mock<SearchesHistoryService>()
+    class FakeSearchesHistoryService(private val history: List<String>) : SearchesHistoryService {
+        override suspend fun lastSearches(): List<String> = history
 
-    private lateinit var manager: ManageSearchQuery
-
-    @Before fun `before each test`() {
-        manager = ManageSearchQuery(searchHistory)
+        override suspend fun registerNewSearch(term: String) = Unit
     }
 
     @Test fun `should return fallback query when no history available`() {
         runBlocking {
-            whenever(searchHistory.lastSearches()).thenReturn(emptyList())
-            assertThat(manager.actualQuery()).isEqualTo(ManageSearchQuery.FALLBACK)
+            val history = emptyList<String>()
+            val service = FakeSearchesHistoryService(history)
+            val manager = ManageSearchQuery(service)
+
+            val actualQuery = manager.actualQuery()
+
+            val expected = ManageSearchQuery.FALLBACK
+            assertThat(actualQuery).isEqualTo(expected)
         }
     }
 
     @Test fun `should return first search query when history available`() {
         runBlocking {
             val history = listOf("Norris", "Dev", "Kotlin")
-            whenever(searchHistory.lastSearches()).thenReturn(history)
-            assertThat(manager.actualQuery()).isEqualTo(history.first())
+            val service = FakeSearchesHistoryService(history)
+            val manager = ManageSearchQuery(service)
+
+            val actualQuery = manager.actualQuery()
+
+            assertThat(actualQuery).isEqualTo("Norris")
         }
     }
 }
