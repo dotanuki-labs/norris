db940c525d9546a839e366a2739e84f6ada69d6c
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-09-07 21:23:52 +0200

    Bumped Gradle Ktlint plugin to 9.4.0

--- buildSrc/src/main/kotlin/BuildPlugins.kt
+++ buildSrc/src/main/kotlin/BuildPlugins.kt
@@ -16,7 +16,6 @@ object BuildPlugins {
         const val androidApplication = "com.android.application"
         const val androidLibrary = "com.android.library"
         const val kotlinJVM = "kotlin"
-        // const val kotlinxSerialization = "kotlinx-serialization"
         const val kotlinxSerialization = "org.jetbrains.kotlin.plugin.serialization"
         const val kotlinAndroid = "kotlin-android"
         const val kotlinAndroidExtensions = "kotlin-android-extensions"
@@ -32,7 +31,7 @@ object BuildPlugins {
     private object Versions {
         const val agp = "4.0.1"
         const val testLogger = "2.1.0"
-        const val ktlint = "9.3.0"
+        const val ktlint = "9.4.0"
         const val jacocoUnified = "0.16.0"
         const val detekt = "1.11.0-RC1"
     }
