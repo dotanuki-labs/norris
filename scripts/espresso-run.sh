cb079dcd92a09d4e94a53128e22f9e544c3a5179
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-09-29 11:04:50 +0200

    Capturing logcat

--- scripts/espresso-run.sh
+++ scripts/espresso-run.sh
@@ -8,15 +8,20 @@ echo "🔥 Install the apks...\n"
 find  . -name "*.apk" -print -exec adb install {} \;
 
 echo "\n🔥 Running instrumentation"
+
+mkdir instrumentation-outputs
+
+adb logcat -d | grep 'norris' | tee instrumentation-outputs/log.txt &
+
 RUNNER="io.dotanuki.demos.norris.test/androidx.test.runner.AndroidJUnitRunner"
-adb shell am instrument -w -r $RUNNER > adb-test.log
+adb shell am instrument -w -r $RUNNER > instrumentation-outputs/adb-test.log
 
 if [ $? -eq 0 ]; then
     echo "\n🔥 Instrumentation test execution failed! Aborting\n"
     exit 1
 fi
 
-cat adb-test.log | grep "INSTRUMENTATION_STATUS: stack=" | grep -v "org.junit.AssumptionViolatedException"
+cat instrumentation-outputs/adb-test.log | grep "INSTRUMENTATION_STATUS: stack=" | grep -v "org.junit.AssumptionViolatedException"
 if [ $? -eq 0 ]; then
   echo "Test failure found"
   exit 1
