9e5f135d92a42c18e673bae67c24c653aeea4157
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-06-16 17:44:36 +0200

    Adding utilities on top of truth

--- platform/testing-commons/build.gradle.kts
+++ platform/testing-commons/build.gradle.kts
@@ -9,4 +9,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.5.0")
     implementation("junit:junit:4.13.2")
+    implementation("com.google.truth:truth:1.1.3")
 }
---
+++ platform/testing-commons/src/main/java/io/dotanuki/testing/truth/EspeculativeExecution.kt
@@ -0,0 +1,13 @@
+package io.dotanuki.testing.truth
+
+import com.google.common.truth.FailureMetadata
+import com.google.common.truth.Subject
+
+class EspeculativeExecution(
+    private val execution: () -> Unit
+) : Subject.Factory<ExpectingErrorSubject, Throwable> {
+    override fun createSubject(
+        metadata: FailureMetadata?,
+        actual: Throwable?
+    ): ExpectingErrorSubject = ExpectingErrorSubject(metadata, actual, execution)
+}
