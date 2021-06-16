277693003f2c76d88d31d25d078633ebd4f66d2a
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-06-16 18:00:46 +0200

    Migrating onboarding module to Truth

--- features/onboarding/src/test/java/io/dotanuki/norris/onboarding/OnboardingViewModelTests.kt
+++ features/onboarding/src/test/java/io/dotanuki/norris/onboarding/OnboardingViewModelTests.kt
@@ -1,17 +1,15 @@
 package io.dotanuki.norris.onboarding
 
 import app.cash.turbine.test
-import io.dotanuki.testing.coroutines.CoroutinesTestHelper
+import com.google.common.truth.Truth.assertThat
 import io.dotanuki.norris.domain.FetchCategories
 import io.dotanuki.norris.domain.model.ChuckNorrisFact
 import io.dotanuki.norris.domain.model.RelatedCategory.Available
 import io.dotanuki.norris.domain.services.CategoriesCacheService
 import io.dotanuki.norris.domain.services.RemoteFactsService
-import io.dotanuki.norris.onboarding.OnboardingScreenState.Idle
-import io.dotanuki.norris.onboarding.OnboardingScreenState.Launching
-import io.dotanuki.norris.onboarding.OnboardingScreenState.Success
+import io.dotanuki.norris.onboarding.OnboardingScreenState.*
+import io.dotanuki.testing.coroutines.CoroutinesTestHelper
 import kotlinx.coroutines.runBlocking
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
