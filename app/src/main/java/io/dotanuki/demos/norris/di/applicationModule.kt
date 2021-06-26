de4fa492c178293cf41192590608fb888cab39db
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-06-26 23:19:40 +0200

    Removing unused module

--- app/src/main/java/io/dotanuki/demos/norris/di/applicationModule.kt
+++ app/src/main/java/io/dotanuki/demos/norris/di/applicationModule.kt
@@ -3,7 +3,6 @@ package io.dotanuki.demos.norris.di
 import io.dotanuki.demos.norris.BuildConfig
 import io.dotanuki.demos.norris.logger.LogcatLogger
 import io.dotanuki.demos.norris.navigation.ScreenLinks
-import kotlinx.coroutines.Dispatchers
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import org.kodein.di.DI
 import org.kodein.di.bind
@@ -28,12 +27,6 @@ val applicationModule = DI.Module("application") {
     bind {
         singleton {
             ScreenLinks.associations
-        }
-    }
-
-    bind {
-        singleton {
-            Dispatchers.IO
         }
     }
 }
