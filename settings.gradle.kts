de596e0e881cd2f88ed65a2ea84bb2168b0b6f70
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-06-27 17:18:41 +0200

    Running CI tasks with custom Gradle cache GHA (#183)
    * Running CI tasks with custom Gradle cache GHA
    
    * Sets remote build cache layer
    
    * Improving some task names
    
    * Removing custom script

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,3 +20,11 @@ include(
     ":features:search",
     ":features:onboarding"
 )
+
+val isCiServer = System.getenv().containsKey("CI")
+
+buildCache {
+    local {
+        isEnabled = !isCiServer
+    }
+}
