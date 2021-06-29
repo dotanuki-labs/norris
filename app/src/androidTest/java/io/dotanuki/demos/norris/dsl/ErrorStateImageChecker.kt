c7ff1c5c3e422731e25b29ee4ad78abb3de58c2c
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-06-29 21:01:23 +0200

    Deleted all Espresso tests

--- app/src/androidTest/java/io/dotanuki/demos/norris/dsl/ErrorStateImageChecker.kt
+++
@@ -1,8 +0,0 @@
-package io.dotanuki.demos.norris.dsl
-
-import com.schibsted.spain.barista.assertion.BaristaImageViewAssertions.assertHasDrawable
-
-class ErrorStateImageChecker(private val imageId: Int) {
-
-    infix fun shows(image: ErrorImage) = assertHasDrawable(imageId, image.resource)
-}
--- app/src/androidTest/java/io/dotanuki/demos/norris/dsl/WriteOnTextInputFieldInteraction.kt
+++
@@ -1,8 +0,0 @@
-package io.dotanuki.demos.norris.dsl
-
-import com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo
-
-class WriteOnTextInputFieldInteraction(private val textFieldId: Int) {
-
-    infix fun received(text: String) = writeTo(textFieldId, text)
-}
