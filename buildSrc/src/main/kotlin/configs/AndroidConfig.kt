ef2250bf19d04bea0dc50932fdc3c8fb6eaad514
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-07-02 01:10:29 +0200

    Bumps minSDK version to 25 (#200)

--- buildSrc/src/main/kotlin/configs/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/configs/AndroidConfig.kt
@@ -7,7 +7,7 @@ object AndroidConfig {
     const val applicationId = "io.dotanuki.demos.norris"
 
     const val compileSdk = 30
-    const val minSdk = 23
+    const val minSdk = 25
     const val targetSdk = compileSdk
 
     const val buildToolsVersion = "30.0.2"
