f1fca7dfa4ee80e1f332a6cba7f108fec8d6e844
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-07-02 12:38:18 +0200

    Removed burster from networking

--- platform/networking/src/test/java/io/dotanuki/norris/networking/NetworkingErrorTransformerTests.kt
+++ platform/networking/src/test/java/io/dotanuki/norris/networking/NetworkingErrorTransformerTests.kt
@@ -1,7 +1,6 @@
 package io.dotanuki.norris.networking
 
 import com.google.common.truth.Truth.assertThat
-import io.dotanuki.burster.using
 import io.dotanuki.norris.networking.CheckErrorTransformation.Companion.checkTransformation
 import io.dotanuki.norris.networking.errors.NetworkingError
 import io.dotanuki.norris.networking.errors.RemoteServiceIntegrationError
@@ -15,34 +14,28 @@ import java.net.UnknownHostException
 
 class NetworkingErrorTransformerTests {
 
-    @Test fun `should transform proper throwable into networking error`() {
-        using<Throwable, NetworkingError> {
-
-            burst {
-                values(UnknownHostException("No Internet"), NetworkingError.HostUnreachable)
-                values(ConnectException(), NetworkingError.HostUnreachable)
-                values(NoRouteToHostException(), NetworkingError.HostUnreachable)
-                values(SocketTimeoutException(), NetworkingError.OperationTimeout)
-                values(IOException("Canceled"), NetworkingError.ConnectionSpike)
-            }
-
-            thenWith { incoming, expected ->
-                checkTransformation(
-                    from = incoming,
-                    using = NetworkingErrorTransformer,
-                    check = { transformed -> assertThat(transformed).isEqualTo(expected) }
-                )
-            }
+    @Test fun `should transform networking error from downstream`() {
+        listOf(
+            UnknownHostException("No Internet") to NetworkingError.HostUnreachable,
+            ConnectException() to NetworkingError.HostUnreachable,
+            NoRouteToHostException() to NetworkingError.HostUnreachable,
+            SocketTimeoutException() to NetworkingError.OperationTimeout,
+            IOException("Canceled") to NetworkingError.ConnectionSpike
+        ).forEach { (thrown, expected) ->
+            assertTransformation(thrown, expected)
         }
     }
 
     @Test fun `should propagate any other error`() {
         val otherError = RemoteServiceIntegrationError.RemoteSystem
+        assertTransformation(otherError, otherError)
+    }
 
+    private fun assertTransformation(target: Throwable, expected: Throwable) {
         checkTransformation(
-            from = otherError,
+            from = target,
             using = NetworkingErrorTransformer,
-            check = { transformed -> assertThat(transformed).isEqualTo(otherError) }
+            check = { transformed -> assertThat(transformed).isEqualTo(expected) }
         )
     }
 }
