f59ea100f2c1247defd2c4004125cd681ae44193
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-07-02 12:47:39 +0200

    Removed burster from features

--- features/search/src/test/java/io/dotanuki/norris/search/tests/SearchQueryValidationTests.kt
+++ features/search/src/test/java/io/dotanuki/norris/search/tests/SearchQueryValidationTests.kt
@@ -1,30 +1,23 @@
 package io.dotanuki.norris.search.tests
 
 import com.google.common.truth.Truth.assertThat
-import io.dotanuki.burster.using
 import io.dotanuki.norris.search.domain.SearchQueryValidation
 import org.junit.Test
 
 class SearchQueryValidationTests {
 
     @Test fun `should validate query`() {
-
-        using<String, Boolean> {
-
-            burst {
-                values("", false)
-                values("a", false)
-                values("u2", false)
-                values("11", false)
-                values("dev", true)
-                values("code", true)
-                values("code more", false)
-                values("thequerytermshouldbesmallerthanthis", false)
-            }
-
-            thenWith { query, validation ->
-                assertThat(SearchQueryValidation.validate(query)).isEqualTo(validation)
-            }
+        listOf(
+            "" to false,
+            "a" to false,
+            "u2" to false,
+            "11" to false,
+            "dev" to true,
+            "code" to true,
+            "code more" to false,
+            "thequerytermshouldbesmallerthanthis" to false
+        ).forEach { (query, validation) ->
+            assertThat(SearchQueryValidation.validate(query)).isEqualTo(validation)
         }
     }
 }
