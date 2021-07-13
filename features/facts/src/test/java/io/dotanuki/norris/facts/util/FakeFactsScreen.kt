4478f1b377404ca5db83c10af211fb698f0b55da
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-07-13 17:40:03 +0200

    Fixing tests

--- features/facts/src/test/java/io/dotanuki/norris/facts/util/FakeFactsScreen.kt
+++ features/facts/src/test/java/io/dotanuki/norris/facts/util/FakeFactsScreen.kt
@@ -1,8 +1,8 @@
 package io.dotanuki.norris.facts.util
 
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import io.dotanuki.norris.facts.presentation.FactsScreenState
-import io.dotanuki.norris.facts.ui.FactsActivity
 import io.dotanuki.norris.facts.ui.FactsScreen
 import io.dotanuki.testing.app.TestApplication
 import org.kodein.di.direct
@@ -13,7 +13,7 @@ class FakeFactsScreen : FactsScreen {
     val trackedStates = mutableListOf<FactsScreenState>()
     var isLinked: Boolean = false
 
-    override fun link(host: FactsActivity, delegate: FactsScreen.Delegate): View {
+    override fun link(host: AppCompatActivity, delegate: FactsScreen.Delegate): View {
         isLinked = true
         return View(host)
     }
