0a96cd263a59b392ffa40bfabfd240de76d77b29
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-07-16 12:17:37 +0200

    Removing unused activity extensions

--- features/facts/build.gradle.kts
+++ features/facts/build.gradle.kts
@@ -24,8 +24,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("androidx.activity:activity:1.3.0-rc01")
-    implementation("androidx.activity:activity-ktx:1.3.0-rc01")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.google.android.material:material:1.4.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
