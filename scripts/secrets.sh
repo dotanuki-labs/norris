1488ea141586fc984ac2fe4fedeaa5d2cfbac03a
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-08-06 19:27:59 +0200

    Revamp signing strategy for release artefacts (#277)
    * Moved proguard files
    
    * Reworked build scripts based on new secrets strategy
    
    * Added Secrets manager script
    
    * Moving to encrypted secrets at repo level
    
    * Updated GHA Workflow
    
    * Changing encryptiong algorithm
    
    * Using gpg to handle crypto instead of openssl
    
    * Fixing Workflow
    
    * Fixing ktlint
    
    * Cleaning up

---
+++ scripts/secrets.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly operation="$1"
+readonly password="$2"
+
+setup() {
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+	cd "${DIR%/*}"
+}
+
+encript_with_gpg() {
+  echo "$1" | \
+  gpg --batch --yes --passphrase-fd 0 --cipher-algo aes256 --symmetric --output "$2" "$3"
+}
+
+decriypt_with_gpg() {
+  echo "$1" | \
+  gpg --decrypt --batch --yes --passphrase-fd 0 --output "$2" "$3"
+}
+
+encrypt() {
+  encript_with_gpg "$1" signing/dotanuki-demos.gpg dotanuki-demos.jks
+  encript_with_gpg "$1" signing/credentials.gpg signing.properties
+}
+
+decrypt() {
+  decriypt_with_gpg "$1" dotanuki-demos.jks signing/dotanuki-demos.gpg
+  decriypt_with_gpg "$1" signing.properties signing/credentials.gpg
+}
+
+setup
+
+case "$operation" in
+    "encrypt")
+        encrypt $password
+        ;;
+    "decrypt")
+        decrypt $password
+        ;;
+    *)
+        echo
+        echo "Error: unsupported operation → $operation"
+        echo "Should pick one from : 'encrypt' or 'decrypt'"
+        echo
+        exit 1
+        ;;
+esac
