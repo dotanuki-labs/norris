2b364499d6ccc8686c7b4da19d9f0ade478caa83
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-08-06 17:37:10 +0200

    Added Secrets manager script

---
+++ scripts/secrets.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly operation="$1"
+readonly password="$2"
+
+setup() {
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+	cd "${DIR%/*}"
+}
+
+encrypt() {
+	openssl enc -aes-256-cbc -pass pass:$1 -in dotanuki-demos.jks -out signing/keystore.enc
+	openssl enc -aes-256-cbc -pass pass:$1 -in signing.properties -out signing/credentials.enc
+}
+
+decrypt() {
+	openssl enc -aes-256-cbc -pass pass:$1 -d -in signing/keystore.enc -out dotanuki-demos.jks
+	openssl enc -aes-256-cbc -pass pass:$1 -d -in signing/credentials.enc -out signing.properties
+}
+
+setup
+
+case "$operation" in
+    "encrypt")
+        encrypt $password
+        ;;
+    "decrypt")
+        decrypt $password
+        ;;
+    *)
+        echo
+        echo "Error: unsupported operation → $operation"
+        echo "Should pick one from : 'encrypt' or 'decrypt'"
+        echo
+        exit 1
+        ;;
+esac
