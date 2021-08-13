4fa754c583e7abf1f70e71ec2c8db6badd47143f
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-08-13 19:29:59 +0200

    Cleaning-up

--- buildSrc/src/main/kotlin/NorrisBuildPlugins.kt
+++
@@ -1,9 +0,0 @@
-object NorrisBuildPlugins {
-
-    object Ids {
-        const val androidApplication = "com.android.application"
-        const val kotlinModule = "io.norris.modules.kotlin"
-        const val androidPlatformModule = "io.norris.modules.android.platform"
-        const val androidFeatureModule = "io.norris.modules.android.feature"
-    }
-}
