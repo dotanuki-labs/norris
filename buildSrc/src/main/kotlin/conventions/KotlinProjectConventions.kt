bc5224636a70254c0fb1474a45af7a3477fddb4a
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-08-14 17:35:38 +0200

    Suppress warnings about Retrofit during test execution

--- buildSrc/src/main/kotlin/conventions/KotlinProjectConventions.kt
+++ buildSrc/src/main/kotlin/conventions/KotlinProjectConventions.kt
@@ -1,15 +1,23 @@
 package conventions
 
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun Project.applyKotlinProjectConventions() {
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
-        kotlinOptions.freeCompilerArgs += listOf(
-            "-Xopt-in=kotlin.time.ExperimentalTime",
-            "-Xopt-in=kotlin.RequiresOptIn"
-        )
+    tasks.run {
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = "11"
+            kotlinOptions.freeCompilerArgs += listOf(
+                "-Xopt-in=kotlin.time.ExperimentalTime",
+                "-Xopt-in=kotlin.RequiresOptIn"
+            )
+        }
+
+        withType<Test>().configureEach {
+            // Fix for retrofit `WARNING: Illegal reflective access by retrofit2.Platform`
+            jvmArgs = listOf("--add-opens", "java.base/java.lang.invoke=ALL-UNNAMED")
+        }
     }
 }
