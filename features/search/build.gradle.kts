b033d492178b12ac937ab8af8fb21c04577241f2
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-10-05 13:03:06 +0200

    Bumps OkHttp libraries to 4.9.2 (#348)

--- features/search/build.gradle.kts
+++ features/search/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     testImplementation(projects.platform.testing.testingRest)
 
     testImplementation("com.google.truth:truth:1.1.3")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.1")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.2")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.robolectric:robolectric:4.6.1")
     testImplementation("androidx.test:core:1.4.0")
--- platform/testing/testing-app/build.gradle.kts
+++ platform/testing/testing-app/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-common:2.3.1")
     implementation("org.kodein.di:kodein-di-jvm:7.8.0")
     implementation("org.kodein.type:kodein-type-jvm:1.8.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.2")
     implementation("org.robolectric:robolectric:4.6.1")
     implementation("androidx.test:core:1.4.0")
     implementation("androidx.test:core-ktx:1.4.0")
