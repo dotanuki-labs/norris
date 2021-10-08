660b83e0c9609c6d91699d192aa3ee0509abd09b
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-10-08 10:15:30 +0200

    Implemented example of simple Composite GHA

---
+++ .github/actions/assign-emulator-profile/emulator-assigner.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env bash
+
+set -e
+
+readonly cyan="\033[1;36m"
+readonly red="\033[1;91m"
+readonly normal="\e[0m"
+
+readonly device="$1"
+
+mapped_profile() {
+    echo "Mapped emulator profile  -> ${cyan}$1${normal}"
+    echo "::set-output name=assigned::$1"
+}
+
+usage() {
+    echo
+    echo -e "Usage : ‣ emulator-assigner.sh ${cyan}device${normal} (nexus4 or pixel)"
+    exit 1
+}
+
+main() {
+    if [[ -z "$device" ]]; then
+        echo -e "${red}Error: no device device supplied${normal}"
+        usage
+    fi
+
+    case "$device" in
+    "pixel")
+        mapped_profile "pixel"
+        ;;
+    "nexus4")
+        mapped_profile "Nexus 4"
+        ;;
+    *)
+        echo -e "${red}Invalid device name -> $device${normal}"
+        usage
+        ;;
+    esac
+}
+
+echo
+main
+echo
