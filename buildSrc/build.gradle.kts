33e498b7887022b163604c98a1697a8f1654f36d
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-10-12 21:03:00 +0200

    Bumps android-gradle-plugin to 7.0.3

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.0.2")
+    implementation("com.android.tools.build:gradle:7.0.3")
     implementation("com.adarshr:gradle-test-logger-plugin:3.0.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     implementation(kotlin("stdlib-jdk8"))
