c6a63869bd567b489e678c84b690aebb0d4136e4
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-10-13 21:09:21 +0200

    Running instrumentation over emulator.wtf

---
+++ scripts/emulator-wtf.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+set -eu
+
+readonly ewtf_home="$GITHUB_WORKSPACE/bin"
+readonly ewtf="$ewtf_home/ew-cli"
+readonly tests_apk="$GITHUB_WORKSPACE/tests-apk/app-release-androidTest.apk"
+readonly target_apk="$GITHUB_WORKSPACE/release-apk/app-release.apk"
+readonly API_TOKEN="$1"
+
+# Setup path
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${DIR%/*}"
+
+# Install Emulator WTF
+
+mkdir -p "$ewtf_home"
+curl https://maven.emulator.wtf/releases/ew-cli -o "$ewtf" && chmod +x "$ewtf"
+
+# List APKs
+
+find . -name "*.apk" -print -exec echo {} \;
+
+# Run tests
+sh "$ewtf" --token "$API_TOKEN" --app "$target_apk" --test "$tests_apk"
+
+
+exit 0
