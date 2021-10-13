a57fc65748eac2d8fef0d6f4a79adc9a14a41875
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-10-13 22:20:19 +0200

    Testing with build matrix

--- scripts/emulator-wtf.sh
+++ scripts/emulator-wtf.sh
@@ -6,7 +6,9 @@ readonly ewtf_home="$GITHUB_WORKSPACE/bin"
 readonly ewtf="$ewtf_home/ew-cli"
 readonly tests_apk="$GITHUB_WORKSPACE/tests-apk/app-release-androidTest.apk"
 readonly target_apk="$GITHUB_WORKSPACE/release-apk/app-release.apk"
-readonly API_TOKEN="$1"
+
+readonly api_token="$1"
+readonly device="$2"
 
 # Setup path
 
@@ -21,10 +23,10 @@ curl -s --fail https://maven.emulator.wtf/releases/ew-cli -o "$ewtf" && chmod +x
 # Run tests
 
 sh "$ewtf" \
-    --token "$API_TOKEN" \
+    --token "$api_token" \
     --app "$target_apk" \
     --test "$tests_apk" \
-    --device model=Pixel2,version=27 \
+    --device model="$device",version=27 \
     --outputs-dir emulator-wtf-results
 
 
