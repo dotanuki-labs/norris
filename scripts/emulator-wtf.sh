d6dc22a778cccfa07ce91f4a8148953c7900aaeb
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-10-14 12:11:57 +0200

    Clean-up

--- scripts/emulator-wtf.sh
+++ scripts/emulator-wtf.sh
@@ -8,7 +8,6 @@ readonly tests_apk="$GITHUB_WORKSPACE/tests-apk/app-release-androidTest.apk"
 readonly target_apk="$GITHUB_WORKSPACE/release-apk/app-release.apk"
 
 readonly api_token="$1"
-readonly device="$2"
 
 # Setup path
 
@@ -16,6 +15,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${DIR%/*}"
 
 # Install Emulator WTF
+
 mkdir -p "$ewtf_home"
 curl -s --fail https://maven.emulator.wtf/releases/ew-cli -o "$ewtf" && chmod +x "$ewtf"
 
@@ -26,8 +26,5 @@ sh "$ewtf" \
     --token "$api_token" \
     --app "$target_apk" \
     --test "$tests_apk" \
-    --device model="$device",version=27 \
+    --device model=Pixel2,version=27 \
     --outputs-dir emulator-wtf-results
-
-
-exit 0
