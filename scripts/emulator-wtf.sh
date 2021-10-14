7080483dbceeea6e41e346de5c11f100b03ad0c2
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-10-14 12:33:17 +0200

    Running Instrumentation tests with emulator.wtf (#356)
    * Running instrumentation over emulator.wtf
    
    * Testing different devices
    
    * Grabbing outputs
    
    * Testing bigger device
    
    * Testing with build matrix
    
    * Clean-up
    
    * Using test orchetrator

---
+++ scripts/emulator-wtf.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+set -eu
+
+readonly ewtf_home="$GITHUB_WORKSPACE/bin"
+readonly ewtf="$ewtf_home/ew-cli"
+readonly tests_apk="$GITHUB_WORKSPACE/tests-apk/app-release-androidTest.apk"
+readonly target_apk="$GITHUB_WORKSPACE/release-apk/app-release.apk"
+
+readonly api_token="$1"
+
+# Setup path
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${DIR%/*}"
+
+# Install Emulator WTF
+
+mkdir -p "$ewtf_home"
+curl -s --fail https://maven.emulator.wtf/releases/ew-cli -o "$ewtf" && chmod +x "$ewtf"
+
+
+# Run tests
+
+sh "$ewtf" \
+    --token "$api_token" \
+    --app "$target_apk" \
+    --test "$tests_apk" \
+    --device model=Pixel2,version=27 \
+    --outputs-dir emulator-wtf-results \
+    --use-orchestrator \
+    --clear-package-data
