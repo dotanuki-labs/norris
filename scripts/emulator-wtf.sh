a12cc7cafbc55e39965df64984f89dee9b180369
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-10-19 23:22:33 +0200

    Running acceptance tests with more emulators (#364)
    * Running acceptance tests over more emulators
    
    * Updates rules for Mergify

--- scripts/emulator-wtf.sh
+++ scripts/emulator-wtf.sh
@@ -8,6 +8,7 @@ readonly tests_apk="$GITHUB_WORKSPACE/tests-apk/app-release-androidTest.apk"
 readonly target_apk="$GITHUB_WORKSPACE/release-apk/app-release.apk"
 
 readonly api_token="$1"
+readonly device_name="$2"
 
 # Setup path
 
@@ -26,7 +27,7 @@ sh "$ewtf" \
     --token "$api_token" \
     --app "$target_apk" \
     --test "$tests_apk" \
-    --device model=Pixel2,version=27 \
+    --device "device=$device_name,version=30,atd=true" \
     --outputs-dir emulator-wtf-results \
     --use-orchestrator \
     --clear-package-data
