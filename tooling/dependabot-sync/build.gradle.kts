c31d2b2226309511d1055a90ad6bc3ca7614e655
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-10-31 22:27:19 +0100

    Filling coordinates on bridge module

--- tooling/dependabot-sync/build.gradle.kts
+++ tooling/dependabot-sync/build.gradle.kts
@@ -1,7 +1,19 @@
+// Bridge to catch updates raised by Dependabot
+// https://github.com/RBusarow/gradle-dependency-sync
+
 plugins {
     id("com.rickbusarow.gradle-dependency-sync") version "0.11.4"
 }
 
 dependencies {
+
+    // Gradle Plugins
+
     dependencySync("com.android.tools.build:gradle:7.0.3")
+    dependencySync("com.adarshr:gradle-test-logger-plugin:3.0.0")
+    dependencySync("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+    dependencySync("org.jetbrains.kotlin:kotlin-serialization:1.5.31")
+    dependencySync("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
+    dependencySync("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.1")
+    dependencySync("com.karumi:shot:5.11.2")
 }
