f9dd16a091161aba7a23f0381bdfd6f839ccefc2
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-11-15 22:45:25 +0100

    Bumps Kotlin Gradle plugins to 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.0")
         classpath("com.adarshr:gradle-test-logger-plugin:3.1.0")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.1")
