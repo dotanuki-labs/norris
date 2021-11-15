95cda9044478117a9d1cf25871851839f23c649e
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2021-11-15 22:29:37 +0100

    Bumps kotlinx.serialization libraries to 1.3.1

--- platform/core/core-rest/build.gradle.kts
+++ platform/core/core-rest/build.gradle.kts
@@ -12,10 +12,10 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.2")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.2")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.3.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.3.0")
 
     testImplementation(projects.platform.testing.testingRest)
 
