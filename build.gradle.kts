72a8de90d7cd3ac469c7b3e542b6fbac70e7e153
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2022-05-18 20:36:51 +0200

    Removes talaiot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     id("io.gitlab.arturbosch.detekt") version "1.20.0"
     id("com.osacky.doctor") version "0.8.0"
-    id("io.github.cdsap.talaiot") version "1.5.2"
     id("org.sonatype.gradle.plugins.scan") version "2.3.0"
 }
 
@@ -31,16 +30,6 @@ doctor {
     GCWarningThreshold.set(0.05f)
     javaHome {
         failOnError.set(false)
-    }
-}
-
-talaiot {
-    metrics {
-        gitMetrics = false
-    }
-
-    publishers {
-        jsonPublisher = true
     }
 }
 
