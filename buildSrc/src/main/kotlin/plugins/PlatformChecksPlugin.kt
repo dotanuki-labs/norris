d165c1e17f0d9306073042c666282ad06d99e958
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2022-08-12 19:03:43 +0200

    Cleaning-up Gradle setup

--- buildSrc/src/main/kotlin/plugins/PlatformChecksPlugin.kt
+++ buildSrc/src/main/kotlin/plugins/PlatformChecksPlugin.kt
@@ -1,7 +1,6 @@
 package plugins
 
 import conventions.ignoredVulnerabilities
-import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.sonatype.gradle.plugins.scan.ossindex.OssIndexPluginExtension
@@ -10,28 +9,7 @@ import shadow.nexus.shadow.org.cyclonedx.model.Component.Type
 
 class PlatformChecksPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.configureKtlint()
-        target.configureDetekt()
         target.configureOssScan()
-    }
-
-    private fun Project.configureKtlint() {
-        plugins.apply("org.jlleitschuh.gradle.ktlint")
-    }
-
-    private fun Project.configureDetekt() {
-        plugins.apply("io.gitlab.arturbosch.detekt")
-
-        val detektExtension = extensions.findByName("detekt") as DetektExtension
-
-        detektExtension.run {
-            config = files("$rootDir/detekt.yml")
-            reports {
-                xml.enabled = false
-                html.enabled = false
-                txt.enabled = false
-            }
-        }
     }
 
     private fun Project.configureOssScan() {
