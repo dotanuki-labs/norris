0f200fe7a8b2c4c6b025d73ca200a34a2deaa87c
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2022-09-08 19:05:18 +0200

    Fix GHA syntax

--- .github/actions/setup-android-build/normalize-jdk-version.sh
+++ .github/actions/setup-android-build/normalize-jdk-version.sh
@@ -7,7 +7,7 @@ readonly requested_jdk="$1"
 case "$requested_jdk" in
     "jdk11" | "jdk17")
         local jdk_version=$(echo "$requested_jdk" | sed "s/jdk//g")
-        echo "::set-output jdk=$jdk_version"
+        echo "::set-output name=jdk::$jdk_version"
         ;;
     "*")
         echo "Invalid jdk name -> $requested_jdk"
