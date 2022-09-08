bbd3a770fa749a29487f2e3a395d7bbbc1953e3b
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2022-09-08 18:50:00 +0200

    Running builds against jdk17 for some GHA jobs

---
+++ .github/actions/setup-android-build/normalize-jdk-version.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+set -e
+
+readonly requested_jdk="$1"
+
+case "$requested_jdk" in
+    "jdk11" | "jdk17")
+        local jdk_version=$(echo "$requested_jdk" | sed "s/jdk//g")
+        echo "::set-output jdk=$jdk_version"
+        ;;
+    "*")
+        echo "Invalid jdk name -> $requested_jdk"
+        exit 1
+        ;;
+esac
