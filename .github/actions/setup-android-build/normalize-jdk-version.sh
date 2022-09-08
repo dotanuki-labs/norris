80c9950f1179ceceba7861c8866c0100b1c9d684
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2022-09-08 19:39:20 +0200

    Fix code style

--- .github/actions/setup-android-build/normalize-jdk-version.sh
+++ .github/actions/setup-android-build/normalize-jdk-version.sh
@@ -16,12 +16,12 @@ if [[ -z "$requested_jdk" ]]; then
 fi
 
 case "$requested_jdk" in
-    "jdk11" | "jdk17")
-        jdk_version=$(echo "$requested_jdk" | sed "s/jdk//g")
-        echo "::set-output name=jdk::$jdk_version"
-        ;;
-    "*")
-        echo "Invalid jdk name -> $requested_jdk"
-        exit 1
-        ;;
+"jdk11" | "jdk17")
+    jdk_version=$(echo "$requested_jdk" | sed "s/jdk//g")
+    echo "::set-output name=jdk::$jdk_version"
+    ;;
+"*")
+    echo "Invalid jdk name -> $requested_jdk"
+    exit 1
+    ;;
 esac
