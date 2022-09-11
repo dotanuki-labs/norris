6ad11de9cbb583ae9ca7ecf44285f81bdb65a4b6
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2022-09-11 20:36:29 +0200

    Makes featureMatrix task cacheable (#654)

--- gradle/plugins/src/main/kotlin/io/dotanuki/norris/gradle/FeatureMatrixTask.kt
+++ gradle/plugins/src/main/kotlin/io/dotanuki/norris/gradle/FeatureMatrixTask.kt
@@ -1,25 +1,47 @@
 package io.dotanuki.norris.gradle
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.Project
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
+@CacheableTask
 abstract class FeatureMatrixTask : DefaultTask() {
 
-    @TaskAction fun evaluateFeatures() {
-        val features =
-            project.rootProject.subprojects
-                .filter { it.layout.projectDirectory.asFile.path.contains("features/") }
-                .map { "\"" + it.layout.projectDirectory.asFile.name + "\"" }
+    @get:InputFiles @get:PathSensitive(PathSensitivity.RELATIVE) abstract val featureModules: ConfigurableFileCollection
 
-        val buildDir = File("${project.rootProject.buildDir}/outputs/matrix").apply {
+    @get:OutputFile abstract val matrixFile: RegularFileProperty
+
+    init {
+        val features = project.rootProject.subprojects.filter {
+            it.layout.projectDirectory.asFile.path.contains("features/")
+        }
+
+        featureModules.from(features.map { it.layout.projectDirectory.asFile })
+        matrixFile.set(project.matrixOutputFile())
+    }
+
+    @TaskAction fun evaluateFeatures() {
+        val featuresJson = featureModules.map { "\"" + it.name + "\"" }
+
+        project.matrixOutputDir().run {
             if (!exists()) mkdirs()
         }
 
-        val matrixFilePath = "$buildDir/features.json"
-        val featureMatrixFile = File(matrixFilePath).apply { createNewFile() }
-        featureMatrixFile.writeText(features.toString())
+        val featureMatrixFile = matrixFile.get().asFile.apply { createNewFile() }
+        featureMatrixFile.writeText(featuresJson.toString())
 
-        logger.lifecycle("Wrote feature matrix file at $matrixFilePath")
+        logger.lifecycle("Wrote feature matrix file at $featureMatrixFile")
     }
+
+    private fun Project.matrixOutputDir(): File = File("${project.rootProject.buildDir}/outputs/matrix")
+
+    private fun Project.matrixOutputFile(): File = File("${matrixOutputDir()}/features.json")
 }
