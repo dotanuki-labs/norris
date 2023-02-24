f03f27ed7cbc54a42a0ae4ad3af4356d656b3913
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-02-24 12:11:46 +0100

    Clears more build-logic warnings

--- gradle/plugins/src/main/kotlin/io/dotanuki/norris/gradle/modules/conventions/KotlinModuleConventions.kt
+++ gradle/plugins/src/main/kotlin/io/dotanuki/norris/gradle/modules/conventions/KotlinModuleConventions.kt
@@ -3,7 +3,6 @@ package io.dotanuki.norris.gradle.modules.conventions
 import io.dotanuki.norris.gradle.modules.models.PlatformDefinitions
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -14,13 +13,6 @@ internal fun Project.applyKotlinProjectConventions() {
     val kotlinCompilerFlags = listOf(
         "-opt-in=kotlin.time.ExperimentalTime",
         "-opt-in=kotlin.RequiresOptIn"
-    )
-
-    // Fix for retrofit :
-    // WARNING: Illegal reflective access by retrofit2.Platform
-    val jvmArgsAdditionalFlags = listOf(
-        "--add-opens",
-        "java.base/java.lang.invoke=ALL-UNNAMED"
     )
 
     tasks.run {
@@ -34,10 +26,6 @@ internal fun Project.applyKotlinProjectConventions() {
         withType<JavaCompile>().configureEach {
             targetCompatibility = javaCompatibility
             sourceCompatibility = javaCompatibility
-        }
-
-        withType<Test>().configureEach {
-            jvmArgs?.addAll(jvmArgsAdditionalFlags)
         }
     }
 }
