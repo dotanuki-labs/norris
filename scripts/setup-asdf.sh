f8dd6272f8716fef4853fc45d2a3e03d125c80f1
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-03-21 07:24:52 +0100

    Managing detekt-cli versions with asdf

---
+++ scripts/setup-asdf.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+set -e
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${dir%/*}"
+
+readonly cyan="\033[1;36m"
+readonly normal="\033[0m"
+
+echo "Setting up local CLI tooling"
+
+if ! which asdf >/dev/null; then
+    echo -e "Error : ${cyan}asdf${normal} required but not available"
+    echo
+    exit 1
+fi
+
+echo
+asdf plugin add ktlint
+asdf plugin add detekt https://github.com/dotanuki-labs/asdf-detekt.git
+asdf install
+echo
+
+echo "Done!"
+echo
